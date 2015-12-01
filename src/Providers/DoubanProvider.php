c01876fce775b77f9ce9923f4887f49809a4b018
overtrue <anzhengchao@gmail.com>
2015-12-01 12:21:06 +0800

    Add Douban provider.

---
+++ src/Providers/DoubanProvider.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * DoubanProvider.php
+ *
+ * This file is part of the socialite.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Overtrue\Socialite\Providers;
+
+use Overtrue\Socialite\AccessTokenInterface;
+use Overtrue\Socialite\User;
+
+
+/**
+ * Class DoubanProvider.
+ */
+class DoubanProvider
+{
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://www.douban.com/service/auth2/auth', $state);
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://www.douban.com/service/auth2/token';
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getUserByToken(AccessTokenInterface $token)
+    {
+        $response = $this->getHttpClient()->get('https://api.douban.com/v2/user/~me', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token->getToken(),
+            ],
+        ]);
+
+        return json_decode($this->removeCallback($response->getBody()->getContents()), true);
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return new User([
+            'id'       => $user['id'],
+            'nickname' => $user['name'],
+            'avatar'   => $user['large_avatar'],
+            'name'     => $user['name'],
+            'email'    => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    protected function getTokenFields($code)
+    {
+        return parent::getTokenFields($code) + ['grant_type' => 'authorization_code'];
+    }
+
+    /**
+     * {@inheritdoc}.
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'form_params' => $this->getTokenFields($code),
+        ]);
+
+        return $this->parseAccessToken($response->getBody()->getContents());
+    }
+
+    /**
+     * Remove the fucking callback parentheses.
+     *
+     * @param mixed $response
+     *
+     * @return string
+     */
+    protected function removeCallback($response)
+    {
+        if (strpos($response, 'callback') !== false) {
+            $lpos = strpos($response, '(');
+            $rpos = strrpos($response, ')');
+            $response = substr($response, $lpos + 1, $rpos - $lpos - 1);
+        }
+
+        return $response;
+    }
+}
