53029e9e4ee9de67d59e39924d68ebcff202ccc0
shoaly <shoaly@qq.com>
2016-12-04 20:20:35 +0800

    提交

--- src/Providers/CorpWeChatProvider.php
+++ src/Providers/CorpWeChatProvider.php
@@ -172,7 +172,7 @@ class CorpWechatProvider extends AbstractProvider implements ProviderInterface
     }
 
     // !!应该尽量不要调用, 除非 单独与overture/wechat使用, 否则同时获取accesstoken, 会冲突
-    public function getLongiveAccessToken($forse_refresh = false)
+    public function getLongiveAccessToken($force_refresh = false)
     {
         $getTokenUrl = $this->getTokenUrl();
         $response = $this->getHttpClient()->get($getTokenUrl, [
