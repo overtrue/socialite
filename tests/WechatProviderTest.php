27752ae251b66fe6608bf4b06fc813ff0b47ed1e
mingyoung <mingyoungcheung@gmail.com>
2017-03-25 00:22:54 +0800

    Shorten variables

--- tests/WechatProviderTest.php
+++ tests/WechatProviderTest.php
@@ -9,41 +9,41 @@ class WechatProviderTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function has_correctly_redirect_response()
     {
-        $wechatRedirectResponse = (new WeChatProvider(Request::create('foo'), 'client_id', 'client_secret', 'http://localhost/socialite/callback.php'))
+        $response = (new WeChatProvider(Request::create('foo'), 'client_id', 'client_secret', 'http://localhost/socialite/callback.php'))
                     ->redirect();
-        $wechatOpenPlatformRedirectResponse = (new WeChatOpenPlatformProvider(Request::create('foo'), 'client_id', ['component-app-id', 'component-access-token'], 'redirect-url'))->redirect();
+        $response2 = (new WeChatOpenPlatformProvider(Request::create('foo'), 'client_id', ['component-app-id', 'component-access-token'], 'redirect-url'))->redirect();
 
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $wechatRedirectResponse);
-        $this->assertStringStartsWith('https://open.weixin.qq.com/connect/qrconnect', $wechatRedirectResponse->getTargetUrl());
-        $this->assertRegExp('|redirect_uri='.urlencode('http://localhost/socialite/callback.php').'|', $wechatRedirectResponse->getTargetUrl());
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $response);
+        $this->assertStringStartsWith('https://open.weixin.qq.com/connect/qrconnect', $response->getTargetUrl());
+        $this->assertRegExp('|redirect_uri='.urlencode('http://localhost/socialite/callback.php').'|', $response->getTargetUrl());
 
-        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $wechatOpenPlatformRedirectResponse);
-        $this->assertStringStartsWith('https://open.weixin.qq.com/connect/oauth2/authorize', $wechatOpenPlatformRedirectResponse->getTargetUrl());
-        $this->assertRegExp('|redirect_uri='.urlencode('redirect-url').'|', $wechatOpenPlatformRedirectResponse->getTargetUrl());
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\RedirectResponse', $response2);
+        $this->assertStringStartsWith('https://open.weixin.qq.com/connect/oauth2/authorize', $response2->getTargetUrl());
+        $this->assertRegExp('|redirect_uri='.urlencode('redirect-url').'|', $response2->getTargetUrl());
     }
 
     /** @test */
     public function providers_has_correct_token_url_and_token_fields()
     {
-        $wechatProvider = new WeChatProvider(Request::create('foo'), 'client_id', 'client_secret', 'http://localhost/socialite/callback.php');
-        $wechatOpenPlatformProvider = new WeChatOpenPlatformProvider(Request::create('foo'), 'client_id', ['component-app-id', 'component-access-token'], 'redirect-url');
+        $provider = new WeChatProvider(Request::create('foo'), 'client_id', 'client_secret', 'http://localhost/socialite/callback.php');
+        $provider2 = new WeChatOpenPlatformProvider(Request::create('foo'), 'client_id', ['component-app-id', 'component-access-token'], 'redirect-url');
 
-        $this->assertEquals('https://api.weixin.qq.com/sns/oauth2/access_token', $wechatProvider->tokenUrl());
+        $this->assertEquals('https://api.weixin.qq.com/sns/oauth2/access_token', $provider->tokenUrl());
         $this->assertSame([
             'appid' => 'client_id',
             'secret' => 'client_secret',
             'code' => 'iloveyou',
             'grant_type' => 'authorization_code',
-        ], $wechatProvider->tokenFields('iloveyou'));
+        ], $provider->tokenFields('iloveyou'));
 
-        $this->assertEquals('https://api.weixin.qq.com/sns/oauth2/component/access_token', $wechatOpenPlatformProvider->tokenUrl());
+        $this->assertEquals('https://api.weixin.qq.com/sns/oauth2/component/access_token', $provider2->tokenUrl());
         $this->assertSame([
             'appid' => 'client_id',
             'component_appid' => 'component-app-id',
             'component_access_token' => 'component-access-token',
             'code' => 'code',
             'grant_type' => 'authorization_code',
-        ], $wechatOpenPlatformProvider->tokenFields('code'));
+        ], $provider2->tokenFields('code'));
     }
 }
 
