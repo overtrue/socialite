7f4ad481a5ca8860aeb2c116836d60618e50853f
overtrue <anzhengchao@gmail.com>
2017-06-29 18:17:03 +0800

    Upgrade Facadebook API version. resolve #75

--- src/Providers/FacebookProvider.php
+++ src/Providers/FacebookProvider.php
@@ -34,7 +34,7 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
      *
      * @var string
      */
-    protected $version = 'v2.5';
+    protected $version = 'v2.9';
 
     /**
      * The user fields being requested.
@@ -87,16 +87,6 @@ class FacebookProvider extends AbstractProvider implements ProviderInterface
         ]);
 
         return $this->parseAccessToken($response->getBody());
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function parseAccessToken($body)
-    {
-        parse_str($body, $token);
-
-        return parent::parseAccessToken($token);
     }
 
     /**
