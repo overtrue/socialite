4e0038a1874656e42f229532c7d9f9c68036a087
uptutu <alexkung@outlook.com>
2020-05-31 14:58:43 +0800

    test(UserTest): refresh token

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -9,7 +9,6 @@
  * with this source code in the file LICENSE.
  */
 
-use Overtrue\Socialite\AccessToken;
 use Overtrue\Socialite\User;
 use PHPUnit\Framework\TestCase;
 
@@ -18,30 +17,27 @@ class UserTest extends TestCase
     public function testJsonserialize()
     {
         $this->assertSame('[]', json_encode(new User([])));
-
-        $this->assertSame('{"token":"mock-token"}', json_encode(new User(['token' => new AccessToken(['access_token' => 'mock-token'])])));
+        $this->assertSame('{"access_token":"mock-token"}', json_encode(new User(['access_token' => 'mock-token'])));
     }
 
     public function test_it_can_get_refresh_token()
     {
         $user = new User([
-            'token' => new AccessToken([
-                'access_token' => 'mock-token',
-                'refresh_token' => 'fake_refresh',
-            ]),
+            'name' => 'fake_name',
+            'access_token' => 'mock-token',
+            'refresh_token' => 'fake_refresh',
         ]);
 
         // 能通过用 User 对象获取 refresh token
         $this->assertSame('fake_refresh', $user->getRefreshToken());
-        // json 序列化只有 token 字段
-        $this->assertSame('{"token":"mock-token"}', json_encode($user));
+        $this->assertSame('{"name":"fake_name","access_token":"mock-token","refresh_token":"fake_refresh"}', json_encode($user));
 
+        // 无 refresh_token 属性返回 null
         $user = new User([]);
-        $user->setToken(new AccessToken([
-            'access_token' => 'mock-token',
-            'refresh_token' => 'fake_refresh',
-        ]));
-        $this->assertSame('fake_refresh', $user->getRefreshToken());
-        $this->assertSame('{"token":"mock-token"}', json_encode($user));
+        $this->assertSame(null, $user->getRefreshToken());
+        // 能通过 setRefreshToken() 设置
+        $user->setRefreshToken('fake_refreshToken');
+        $this->assertSame('fake_refreshToken', $user->getRefreshToken());
+        $this->assertSame('{"refresh_token":"fake_refreshToken"}', json_encode($user));
     }
 }
