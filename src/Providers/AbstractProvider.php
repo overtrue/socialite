1be44823795e6659919fdfbf42d0bb14fd30fccc
overtrue <anzhengchao@gmail.com>
2020-06-08 19:35:47 +0800

    Set default scopes with config.

--- src/Providers/AbstractProvider.php
+++ src/Providers/AbstractProvider.php
@@ -82,6 +82,7 @@ abstract class AbstractProvider implements ProviderInterface
     public function __construct(array $config)
     {
         $this->config = new Config($config);
+	$this->scopes = $config['scopes'] ?? [];
         $this->redirectUrl = $this->config->get('redirect_url') ?? $this->config->get('redirect');
     }
 
