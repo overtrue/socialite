dcbb1eed948fe036e6de8cdf0b125f5af1bc73fb
woei66 <woei66@gmail.com>
2023-09-01 19:01:34 +0800

    add picture field for user's avatar (#267)

--- src/Providers/Facebook.php
+++ src/Providers/Facebook.php
@@ -17,7 +17,7 @@ class Facebook extends Base
 
     protected string $version = 'v3.3';
 
-    protected array $fields = ['first_name', 'last_name', 'email', 'gender', 'verified'];
+    protected array $fields = ['first_name', 'last_name', 'email', 'gender', 'verified','picture'];
 
     protected array $scopes = [];
 
